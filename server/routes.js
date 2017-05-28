2299bcd676d015d5222cb29f3146f78f49e40000
Charpell <charpellumeh@gmail.com>
2017-05-28 06:35:08 +0100

    Persist with firebase

--- server/routes.js
+++ server/routes.js
@@ -20,6 +20,19 @@ router.post('/user/signup', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+        // add element to database
+      usersRef.push({
+        username: username,
+        password: password,
+        email: user.email
+      });
+      res.json({ message: `The user ${username}, was created and saved` });
+    })
+    .catch((error) => {
+      res.json(error);
+    });
 });
 
 module.exports = router;
