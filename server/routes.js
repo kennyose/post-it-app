e77a81b3c179bfcaa46c730027097c80c2b2cd5e
Charpell <charpellumeh@gmail.com>
2017-05-28 06:47:30 +0100

    Create signin route

--- server/routes.js
+++ server/routes.js
@@ -36,5 +36,9 @@ router.post('/user/signup', (req, res) => {
 });
 
 
-
+// Sign In
+router.post('/user/signin', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+});
 module.exports = router;
