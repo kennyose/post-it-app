7b10dd792849f16582c50cb4fbc187a16617dd74
Charpell <charpellumeh@gmail.com>
2017-05-28 06:48:01 +0100

    Retrive data from firebase

--- server/routes.js
+++ server/routes.js
@@ -40,5 +40,13 @@ router.post('/user/signup', (req, res) => {
 router.post('/user/signin', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+   firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((user) =>{
+        res.json({ message: `The user ${email}, has logged in` });
+        })
+        .catch((error) =>{
+            res.json(error);
+        });
+        
 });
 module.exports = router;
