5d25120d2e2286f067752fe20461b15f53c565d6
Charpell <charpellumeh@gmail.com>
2017-05-31 16:42:29 +0100

    Refactor routes.js

--- server/app.js
+++ server/app.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const routes = require('./routes');
+const path = require('path');
+
+
 const port = process.env.PORT || 3000;
+
 const app = express();
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Serve bundled client files
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 
 // Root API endpoint
 app.use('/', routes);
- 
+
 
 //This gets route for webpage
 //test of server.===========================================
