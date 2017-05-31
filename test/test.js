a64a07081fe571f1747ec2b2f8b4bb542fe3a5a2
Charpell <charpellumeh@gmail.com>
2017-05-31 23:23:44 +0100

    Write test for SignIn route

--- test/test.js
+++ test/test.js
@@ -7,12 +7,32 @@ const userSignUp = {
   email: 'ebuka@gmail.com'
 };
 
+const userSignIn = {
+  email: 'ebuka@gmail.com',
+  password: '12345ebuka'
+};
+
+
 
 describe('SignUp Route', () => {
   it('This user should signup with his/her details', (done) => {
     request(app)
       .post('/user/signup')
       .send(userSignUp)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+describe('SignIn Route', () => {
+  it('The user should be able to signin', (done) => {
+    request(app)
+      .post('/user/signin')
+      .send(userSignIn)
       .set('Accept', 'application/json')
       .expect(200)
       .end((err) => {
