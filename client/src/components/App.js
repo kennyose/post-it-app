dbbaa7242e015ce888fd2107ef28f1ead767232e
Charpell <charpellumeh@gmail.com>
2017-06-01 12:52:41 +0100

    Convert CSS to SASS

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 
-const App = (props) => {
+import {Switch, Route, Redirect, Link} from 'react-router-dom';
+
+import Signin from './Signin'
+import Signup from './Signup';
+
+
+const App = () => {
     return (
         <div>
-            <h1>Welocme to PostIt</h1>
+             <header>
+          <div className="container">
+            <div id="branding">
+              <h1>
+                <span className="highlight">PostIt
+                </span>Meseenger App</h1>
+            </div>
+            <nav>
+              <ul>
+                <li>
+                  <a href>Home</a>
+                </li>
+                <li>
+                  <Link to='/user/signin'>Sign In</Link>
+                </li>
+                <li>
+                  <Link to='/user/signup' >Sign Up</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <img src="https://pbs.twimg.com/media/DASTsaoW0AA_rLW.jpg" />
+
+        <Switch>
+          <Route exact path='/user/signup' component={Signup}/>
+          <Route path='/user/signin' component={Signin}/>
+        </Switch>
+
+        <footer>
+          <p>Andela, Copyright © 2017</p>
+        </footer>
+            
+         
+       
         </div>
     )
 }
