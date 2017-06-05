edbc66f7ff0f4ec860e329372781d66f6796a784
Charpell <charpellumeh@gmail.com>
2017-06-05 06:24:50 +0100

    Refactor Authentication file

--- client/src/js/authentication/authentication.js
+++ client/src/js/authentication/authentication.js
@@ -25,10 +25,13 @@ export function logout () {
 export function login (email, pw) {
   return firebaseAuth().signInWithEmailAndPassword(email, pw)
     .catch(function(error) {
+      
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
+
   if (errorCode === 'auth/wrong-password') {
+
     alert('Wrong password.');
   } else {
     alert(errorMessage);
